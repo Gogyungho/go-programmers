c7b547a3a7f4908a6f54dbe69186c49931c91174
goguard_Dev <61042217+Gogyungho@users.noreply.github.com>
2023-01-15 15:14:13 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,7 @@
+function solution(age) {
+    let alp = {0 : "a",1 : "b",2 : "c",3 : "d",4 : "e",5 : "f",6 : "g",7 : "h",8 : "i",
+        9 : "j"};
+    let newAge = age + "";
+    
+    return newAge.split("").map((el) => alp[el]).join("");
+}
